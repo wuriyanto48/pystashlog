428d06ed3d97fbab4150e44ac17af986d32d1771
Wuriyanto <wuriyanto48@yahoo.co.id>
2020-11-03 09:19:50 +0700

    add package setup

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import pystashlog
+import pathlib
+
+HERE = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (HERE / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='pystashlog', 
+    version=pystashlog.__version__,
+    description='Logstash client library for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/wuriyanto48/pystashlog',
+    author='telkomdev',
+    author_email='wuriyanto48@yahoo.co.id',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='logstash, elk, elastic, logstash client, kibana',
+    packages=find_packages(where='pystashlog', exclude=('tests', 'env', 'elk')),
+    python_requires='>=3.5',
+    project_urls={
+        'Bug Reports': 'https://github.com/wuriyanto48/pystashlog/issues',
+        'Source': 'https://github.com/wuriyanto48/pystashlog/',
+    },
+)
