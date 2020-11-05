5eb70bd3353197cecf45add7d73bb30d1716c8fb
Wuriyanto <wuriyanto48@yahoo.co.id>
2020-11-05 10:03:15 +0700

    update setup.py, update topic

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Client Library :: TCP :: ELK :: Logstash',
 
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
